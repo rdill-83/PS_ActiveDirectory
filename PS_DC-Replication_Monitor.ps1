c9ff0132cd39acf551a06e041c0d516cc2bb4b19
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-07-10 23:17:02 -0700

    Update PS_DC-Replication_Monitor.ps1
    Syntax Update

--- PS_DC-Replication_Monitor.ps1
+++ PS_DC-Replication_Monitor.ps1
@@ -1,3 +1,3 @@
 # Powershell RepAdmin - ReplicationPartner Monitor
 
-Get-ADReplicationPartnerMetadata -target * -Partition Domain | Sort Server | Select Server, @{n="Partner";e={(Resolve-DNSName $_.PartnerAddress).NameHost}}, LastReplicationAttempt,LastReplicationSuccess,LastReplicationResult,PartnerType,IntersiteTransportType,ScheduledSync | Out-GridView -Title "PS Replication Monitor - $(Get-Date -Format yyyy-MM-dd-hh-mm)"	
+Get-ADReplicationPartnerMetadata -Target * -Partition Domain | Sort Server | Select Server, @{n="Partner";e={(Resolve-DNSName $_.PartnerAddress).NameHost}}, LastReplicationAttempt,LastReplicationSuccess,LastReplicationResult,PartnerType,IntersiteTransportType,ScheduledSync | Out-GridView -Title "PS Replication Monitor - $(Get-Date -Format yyyy-MM-dd-hh-mm)"	
