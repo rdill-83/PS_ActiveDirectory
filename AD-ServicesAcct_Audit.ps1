af1f1e4904a3acd90f7619d7f3e1812046bc647f
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-07-16 00:02:47 -0700

    Update AD-ServicesAcct_Audit.ps1
    ping response branch

--- AD-ServicesAcct_Audit.ps1
+++ AD-ServicesAcct_Audit.ps1
@@ -6,11 +6,17 @@
 # List of Server Nodes to Variable (filtering to all Servers in domain):
 $Servers = (Get-ADComputer -Filter * -Properties * | Where {$_.OperatingSystem -like '*server*'} | Sort Name).Name
 
-
 foreach ($Server in $Servers)
 {
-    $ServerName = $Server
+$ServerName = $Server
+
+if (Test-Connection -ComputerName $ServerName -Count 1 -Quiet)
+{   
     $Services = GWMI Win32_Service -ComputerName $ServerName 
     $ServicesInfo = $Services | where {$_.StartName -notlike 'localSystem' -and $_.Startname -notlike 'NT Authority*' -and $_.StartName -notlike $null} | Select-Object PSComputerName,Name,Caption,StartName,StartMode
     $ServicesInfo | Export-CSV -Path "C:\_IT-Temp\$(Get-Date -f yyyy-MM-dd)_Server_ServiceAcct_Audit.csv" -Append
+}
+else
+{
+    Write-Host "Server $ServerName is Not Online
 }
