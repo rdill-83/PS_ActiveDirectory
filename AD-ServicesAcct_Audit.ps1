d2d163798c1025d0b32a48d01d0a35be8e2491a7
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-07-15 23:50:32 -0700

    Update AD-ServicesAcct_Audit.ps1
    Initialize Guts of Script

--- AD-ServicesAcct_Audit.ps1
+++ AD-ServicesAcct_Audit.ps1
@@ -1,3 +1,16 @@
 # AD-ServicesAcct_Audit.ps1
 # Author RD
 
+# Audit Windows Servers for Accts associated w/ Windows Services
+
+# List of Server Nodes to Variable (filtering to all Servers in domain):
+$Servers = (Get-ADComputer -Filter * -Properties * | Where {$_.OperatingSystem -like '*server*'} | Sort Name).Name
+
+
+foreach ($Server in $Servers)
+{
+    $ServerName = $Server
+    $Services = GWMI Win32_Service -ComputerName $ServerName 
+    $ServicesInfo = $Services | where {$_.StartName -notlike 'localSystem' -and $_.Startname -notlike 'NT Authority*' -and $_.StartName -notlike $null} | Select-Object PSComputerName,Name,Caption,StartName,StartMode
+    $ServicesInfo | Export-CSV -Path "C:\_IT-Temp\$(Get-Date -f yyyy-MM-dd)_Server_ServiceAcct_Audit.csv" -Append
+}
