70fa46e2e4380072f8d48b774c5d5578ac066ed7
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-07-16 00:05:46 -0700

    Update AD-ServicesAcct_Audit.ps1

--- AD-ServicesAcct_Audit.ps1
+++ AD-ServicesAcct_Audit.ps1
@@ -12,7 +12,7 @@ $ServerName = $Server
 
 if (Test-Connection -ComputerName $ServerName -Count 1 -Quiet)
 {   
-    $Services = GWMI Win32_Service -ComputerName $ServerName 
+    $Services = GWMI Win32_Service -ComputerName $ServerName -ErrorAction SilentlyContinue
     $ServicesInfo = $Services | where {$_.StartName -notlike 'localSystem' -and $_.Startname -notlike 'NT Authority*' -and $_.StartName -notlike $null} | Select-Object PSComputerName,Name,Caption,StartName,StartMode
     $ServicesInfo | Export-CSV -Path "C:\_IT-Temp\$(Get-Date -f yyyy-MM-dd)_Server_ServiceAcct_Audit.csv" -Append
 }
