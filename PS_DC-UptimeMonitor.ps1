b1d569eda844092ed46169e571f892f07c8f13ce
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-12-12 22:18:38 -0800

    23-12-12 Commit
    Syntax - Update PS_DC-UptimeMonitor.ps1

--- PS_DC-UptimeMonitor.ps1
+++ PS_DC-UptimeMonitor.ps1
@@ -1,7 +1,7 @@
 # DC - Uptime Monitor
 
 # Retrieve DCs to Array:
-$DC = (Get-ADDomainController -Filter * | Sort Name).Name
+$DCs = (Get-ADDomainController -Filter * | Sort Name).Name
 
 # Uptime Monitor:
-GWMI -Class Win32_OperatingSystem -CN $DC | FT -Property CSName,@{n='Last Bootup Time';e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}} -Auto
+GWMI -Class Win32_OperatingSystem -CN $DCs | FT -Property CSName,@{n='Last Bootup Time';e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}} -Auto
