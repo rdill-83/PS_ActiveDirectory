76b2137ecc6a95ff1504e375c7183cdf3a98478e
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-12-12 22:32:24 -0800

    23-12-12 Commit
    Update PS_DC-Service_Monitor.ps1

--- PS_DC-Service_Monitor.ps1
+++ PS_DC-Service_Monitor.ps1
@@ -1,2 +1,10 @@
 # DC Service Monitor
 
+# Domain Controllers to Array:
+$DCs = (Get-ADDomainController -Filter * | Sort Name).Name
+
+# Domain Controller Service Status Query:
+Get-Service -ComputerName $DCs -Name ADWS,DNS,KDC,Netlogon | FT MachineName,Name,DisplayName,Status,StartType
+
+# DC Service Status Query w/ DHCP Server
+Get-Service -ComputerName $DCs -Name ADWS,DHCPServer,DNS,KDC,Netlogon | FT MachineName,Name,DisplayName,Status,StartType
